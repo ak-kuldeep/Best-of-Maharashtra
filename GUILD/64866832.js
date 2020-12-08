138179c9b59ce3fcf0f2d6cc9745a5880218459c
ak-kuldeep <kuldeepgadhe@gmail.com>
2020-12-08 18:22:39 +0530

    1.020

--- GUILD/64866832.js
+++ GUILD/64866832.js
@@ -1,8 +1,12 @@
-const NjQ4NjY4MzI = {
-    name: 'DadasGroup',
-    id: '64866832',
-    glory: '100000',
-    create: '20060723',
-
-    '1993944111': 'M',
+var wcphGd = this;
+wcphGd.NjQ4NjY4MzI = {
+    20200928: {
+        '1993944111': 'M',
+        name: 'DadasGroup',
+    },
+    c: {
+        id: '64866832',
+        glory: '100000',
+        create: '20060723',
+    }
 }
