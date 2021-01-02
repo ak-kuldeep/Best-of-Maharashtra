447c15b14ec563a783648bf4057d904b4e7ff4c5
ak-kuldeep <kuldeepgadhe@gmail.com>
2021-01-02 21:06:39 +0530

    1.025

--- GUILD/NjQ4NjY4MzI.js
+++ GUILD/NjQ4NjY4MzI.js
@@ -1,12 +1,12 @@
 var wcphGd = this;
 wcphGd.NjQ4NjY4MzI = {
     20200928: {
-        '1993944111': 'M',
-        name: 'DadasGroup',
+        1993944111: "M",
+        name: "DadasGroup",
     },
     c: {
-        id: '64866832',
-        glory: '100000',
-        create: '20060723',
-    }
-}
+        id: "64866832",
+        glory: "100000",
+        create: "20060723",
+    },
+};
